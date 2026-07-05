20918e56c305a1210f262750aee4c9690405a2e1
Popescu Gabi <a.gabia385@gmail.com>
2026-07-05 23:57:30 +0300

    Added Category controller and visualisation, fixed small issue with the form in package/new.html.twig

---
+++ src/Controller/CategoryController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Category;
+use App\Form\CategoryFormType;
+use App\Repository\CategoryRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\Request;
+
+final class CategoryController extends AbstractController
+{
+    #[Route('/category', name: 'app_category')]
+    public function index(CategoryRepository $categoryRepository): Response
+    {
+        $categories = $categoryRepository->findAll();
+        return $this->render('category/index.html.twig', [
+            'controller_name' => 'CategoryController',
+            'categories' => $categories,
+        ]);
+    }
+
+    #[Route('/category/{id}', name: 'app_category_view')]
+    public function view(int $id, CategoryRepository $categoryRepository): Response
+    {
+        $category = $categoryRepository->find($id);
+
+        return $this->render('category/view.html.twig', [
+            'category' => $category,
+        ]);
+    }
+
+    #[Route('/new/category', name: 'app_category_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager ): Response
+    {
+        $category = new Category();
+
+        $form = $this->createForm(CategoryFormType::class, $category);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($category);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_category');
+        }
+
+        return $this->render('category/new.html.twig', [
+            'form' => $form,
+        ]);
+    }
+}
