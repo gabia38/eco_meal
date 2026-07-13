b7f1acee8b4bb63aff0bb32df2349a227c4f6816
Popescu Gabi <a.gabia385@gmail.com>
2026-07-13 13:51:38 +0300

    Added regex password constraints

--- src/Form/UserRegistrationFormType.php
+++ src/Form/UserRegistrationFormType.php
@@ -8,6 +8,7 @@ use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Regex;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
@@ -36,11 +37,15 @@ class UserRegistrationFormType extends AbstractType
                         message: 'Please enter a password',
                     ),
                     new Length(
-                        min: 6,
+                        min: 8,
                         minMessage: 'Your password should be at least {{ limit }} characters',
                         // max length allowed by Symfony for security reasons
                         max: 4096,
                     ),
+                    new Regex(
+                        pattern: '/(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[^a-zA-Z\d]).+/',
+                        message: "You should have at least one lower case and one upper case letter, one digit and one special sign",
+                    )
                 ],
             ])
             ->add('consumer', ConsumerFormType::class,[
